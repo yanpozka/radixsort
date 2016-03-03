a6f1bc247ea1dd157379fd473c100772f064022e
Yandry Pozo <yandry.castillo@hpe.com>
2016-03-03 14:49:12 -0700

    Digit on specific position

--- radixsort.go
+++ radixsort.go
@@ -2,10 +2,29 @@ package radixsort
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
+type RadixInterface interface {
+	sort.Interface
+	Digit(pos int) int
+}
+
+type IntRadixSlice []int
+
+func (s IntRadixSlice) Len() int           { return len(s) }
+func (s IntRadixSlice) Less(i, j int) bool { return s[i] < s[j] }
+func (s IntRadixSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+func (s IntRadixSlice) Digit(i, pos int) int {
+	if pos <= 0 || pos > 70 {
+		return -1
+	}
+	return (s[i] / int(math.Pow10(pos-1))) % 10
+}
+
 //
-func RadixSort(i sort.Interface) {
-	fmt.Println(i)
+func RadixSort(data sort.Interface) {
+	fmt.Println(data)
 }
